bcfaaadfd1265b697cef2cf1883432068048fd4e
Rodrigo <rodrigoleonavas@gmail.com>
2012-09-26 21:10:47 -0300

    adding portal init

---
+++ trunk/src/portal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = 'b43d791ec789e9f88764d45adc0ff44cf7c55fba9dacccf55c2ee420fc52016bd354db7e80312706bbd51e816cc9a3e625122a70eb6e4854b68ad6577c7fd51a'
